030189dd638e3265c3ab886d2839b5897d4e7c30
hns-dev <4thewin4441@gmail.com>
2022-08-08 23:31:50 +0300

    Add a route to serve the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const todoRoutes = require("./routes/todos");
@@ -13,6 +14,14 @@ app.use(express.json());
 
 // routes
 app.use("/api/todos", todoRoutes);
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "../", "frontend", "build", "index.html")
+  )
+);
 
 // connect to database
 mongoose
